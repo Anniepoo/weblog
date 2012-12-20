3960813d70d30aff531776d64cc2d21decdf5ebd
Anniepoo <annie66us@yahoo.com>
2012-12-19 17:34:45 -0800

    check in work performed when this was private lib of University of Houston Dept. of Health and Human Performance project.

---
+++ debug_page.pl
@@ -0,0 +1,40 @@
+:- module(debug_page, []).
+/**   <module>  Debugging Page
+
+Displays generally useful debug information.
+
+ * List of registered handlers and paths
+ * Current File Search Path
+ * Current Session info
+ * Info from
+ [[stats.pl]]
+
+*/
+/* realized I needed ClioPatria for stats.pl
+
+:- use_module(library(http/html_write)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_session)).
+:- use_module(library(server_stats)).
+:- use_module(layout).
+
+:- http_handler(admin(debugpage) , debug_page_handler, [id(debug_page)]).
+
+debug_page_handler(Request) :-
+	reply_html_page(
+	    hhp_web_style,
+	    title('HHP Virtual Web App Debugging Page'),
+	    debug_contents).
+
+% dont use abox here, want to keep this separated
+debug_contents -->
+	\(layout:abox('table', 'Session Info',
+		     \debug_page:all_session_info)),
+	\(layout:abox('table', 'All File Handlers',
+		     \debug_page:all_handlers)),
+	\(layout:abox('table', 'Current File Search Path',
+		     \debug_page:current_fsp)),
+
+		     http_dispatch:handler(Path, Pred, true, Options)
+ */
+
