dcf1415c50c31d3801fb956dd1e327c53ef4d419
Anniepoo <annie66us@yahoo.com>
2013-06-11 15:40:09 -0700

    debugged geohashing

--- formatting/README.md
+++ formatting/README.md
@@ -2,7 +2,29 @@
 
 This directory contains modules useful for controlling page layout.
 
-Nothing in this directory should be loaded or used by code.
+This of course includes things like the table tool, but it also includes
+some tools for dynamic bits of page motion like tabs you can slide out
+to reveal extra info, lightboxes (the 'dialog' thing that darkens the rest of the page),
+overlapping 'windows' (like a desktop), draggable rearrangeable objects.
+
+In short, if you're wondering whether something belongs here or in nav,
+if it's primary purpose is to navigate around the site, it belongs in nav.
+If it's primary purpose is to deal with a lack of screen real estate
+or to help layout a page, it belongs here.
+
+Yes, that's vague. 
+
+Current modules:
+
+boxes - supports visual boxes on screen with headers
+
+tables - support for tables of data
+
+tabs - support for slideout tabs
+
+
+
+
 
 
 
--- info/geohashing/geohashing.pl
+++ info/geohashing/geohashing.pl
@@ -120,29 +120,37 @@ frac_part --> ".", digits(_).
 hash_point(Date, Grat, Point) :-
 	a_hash_point(Date, Grat, Point),!.
 
-hash_point(YY - MM - DD, graticule(Lat, Long), point(PLat, PLong)) :-
-	LLat is float_integer_part(Lat),
+hash_point(Date, Grat, Pt) :-
+	hash_atom( Date, Grat, HashAtom),
+	point_from_atom(HashAtom, Grat, Pt),
+	asserta(a_hash_point(Date, Grat, Pt)).
+
+hash_atom( YY - MM - DD, graticule(_, Long), ToHash) :-
 	LLong is float_integer_part(Long),
 	(   YY < 1000 -> YYYY is YY + 2000 ; YYYY is YY),
 	(   east_rule(YYYY - MM - DD, LLong) -> DDDD is DD - 1; DDDD is DD),
-	normalize_geohash_date(YYYY - MM - DDDD, ZY - ZM - ZD),
+	normalize_geohash_date(YYYY - MM - DD, ZY - ZM - ZD),
 	(   ZM < 10 -> MFill = '0' ; MFill = ''),
 	(   ZD < 10 -> DFill = '0' ; DFill = ''),
-	crstock_stats(djia, opening, ZY - ZM - ZD, Stock),
+	normalize_geohash_date(YYYY - MM - DDDD, StockY - StockM - StockD),
+	crstock_stats(djia, opening, StockY - StockM - StockD, Stock),
 	format(atom(ToHash), '~d-~w~d-~w~d-~2f',
-	       [ZY, MFill, ZM, DFill, ZD, Stock]),
+	       [ZY, MFill, ZM, DFill, ZD, Stock]).
+
+point_from_atom(ToHash, graticule(Lat, Long), point(PLat, PLong)) :-
+	LLat is float_integer_part(Lat),
+	LLong is float_integer_part(Long),
 	% The only available MD5 *DIGEST* implementation is from semweb
 	rdf_atom_md5(ToHash, 1, HashedAtom),
 	atom_codes(HashedAtom, HashedCodes),
 	append(FirstHalf, LastHalf, HashedCodes),
 	same_length(FirstHalf, LastHalf),!,
         hex_codes_frac_float(FirstHalf, 1.0, LatFrac),
-	hex_codes_frac_float(LastHalf, 1.0, InvLongFrac),
-	LongFrac is 1.0 - InvLongFrac,  % heaven knows
+	hex_codes_frac_float(LastHalf, 1.0, LongFrac),
 	grat_geo(LLat , LatFrac, PLat),
-	grat_geo(LLong, LongFrac, PLong),
-	asserta(a_hash_point( YY - MM - DD,
-			      graticule(Lat, Long), point(PLat, PLong))).
+	grat_geo(LLong, LongFrac, PLong).
+
+
 
 /**   globalhash(+Date:date,  -Point:point) is semidet
 
@@ -254,27 +262,141 @@ hex_codes_frac_float([H|T], Place, Value) :-
 	Value is NewPlace * D + RemVal.
 
 
+/*                     Test suite below here     */
 
+%  main entry point for testing
 test_geohashing :-
+	retractall(croxstock:stock_stat(_,_,_,_)),
 	retractall(a_hash_point(_,_,_)),!,
+	test_hash_end_end,!,
+	test_east_rule,!,
+	test_grat_geo,!,
+	test_hashatom,!,
+	test_hex_frac,!,
+	format('finished~n').
+
+% test conversion from hex string fraction to float fraction
+test_hex_frac :-
+	test_case_hex_frac(Hex, Frac),
+	call(hex_codes_frac_float(Hex, 1.0, GotFrac)),
+	(   abs(GotFrac - Frac) < 0.0001 -> true
+	;   format('hex_codes ~s  expected ~w got ~w~n', [Hex, Frac, GotFrac])
+	),
+	fail.
+test_hex_frac.
+
+test_case_hex_frac("db9318c2259923d0", 0.857713).
+test_case_hex_frac("8b672cb305440f97", 0.544543).
+test_case_hex_frac("ffffffffffffffff", 0.999999).
+test_case_hex_frac("0000000000000000", 0.0).
+test_case_hex_frac("8000000000000000", 0.5).
+
+% test converting graticule and fractional location to geo coord
+test_grat_geo :-
+	test_case_gg(GratNum, Frac, Geo),
+	call(grat_geo(GratNum, Frac, GotGeo)),
+	(   GotGeo =:= Geo -> true
+	;   format('grat_geo case ~w should be ~w~n', [grat_geo(GratNum, Frac, GotGeo), Geo])
+	),
+	fail.
+test_grat_geo.
+
+test_case_gg(40, 0.56, 40.56).
+test_case_gg(40.0, 0.87, 40.87).
+test_case_gg(0.0 , 0.66, 0.66).
+test_case_gg(-0.0, 0.66, -0.66).
+test_case_gg(-2.0, 0.45, -2.45).
+
+
+% test the east rule rule
+test_east_rule :-
+	test_east_data(Dt, Long, true),
+	east_rule(Dt, Long),
+	(   east_rule(Dt, Long) ->
+	    true
+	;
+	    format('eastrule should apply to ~w ~w but doesnt~n', [Dt, Long])
+	),
+	fail.
+test_east_rule :-
+	test_east_data(Dt, Long, false),
+	(   east_rule(Dt, Long) ->
+	    format('eastrule should not apply to ~w ~w but does~n', [Dt, Long])
+	),
+	fail.
+test_east_rule.
+
+test_east_data( 2008 - 5 - 20, 0, false).
+test_east_data( 2013 - 5 - 28, -31, false).
+test_east_data( 2013 - 5 - 28, 0, true).
+
+
+% Test the entire hashpoint generation
+test_hash_end_end :-
 	test_item(Y-M-D, GLat, GLong, Lat, Long),
 	hash_point(Y-M-D, graticule(GLat, GLong), point(PLat, PLong)),
 	Dist is sqrt((Lat - PLat) * (Lat - PLat) +
 		     (Long - PLong) * (Long - PLong)),
-	(   Dist > 0.1 ->
+	(   Dist > 0.0001 ->         % 36 ft at equator
 	    format('~w-~w-~w_~w_~w should be ~w,~w   but is ~w,~w~n',
 		   [Y,M,D,GLat,GLong,Lat,Long,PLat,PLong])
 	;   true),
-	false.
+	fail.
+test_hash_end_end.
 
-test_item(2013-5-31, 50, 8, 50.47615093, 8.75973636).
-test_item(2013-5-31, 52, 1, 52.47615093, 1.75973636).
-test_item(2013-5-31, 52, -0.0, 52.47615093, -0.75973636).
-test_item(2013-5-31, 52, 0.0, 52.47615093, 0.75973636).
-test_item(2013-5-31, 52, -1.0, 52.47615093, -1.75973636).
+test_item(2013 - 5 - 31, 50, 8, 50.47615093, 8.75973636).
+test_item(2013 - 5 - 31, 52, 1, 52.47615093, 1.75973636).
+test_item(2013 - 5 - 31, 52, -0.0, 52.47615093, -0.75973636).
+test_item(2013 - 5 - 31, 52, 0.0, 52.47615093, 0.75973636).
+test_item(2013 - 5 - 31, 52, -1.0, 52.47615093, -1.75973636).
+test_item(2013 - 6 - 6, 37, -120, 37.64757933, -120.17135344).
+test_item(2013 - 6 - 6, 52, -2, 52.13259239, -2.18860213).
+test_item(2013 - 6 - 6, 52, 2, 52.13259239, 2.18860213).
+
+%	The 30W compliance test
+test_item(2008 - 5 - 20, 68, -30, 68.63099, -30.61895 ).
+test_item(2008 - 5 - 21, 68, -30, 68.17947, -30.86154 ).
+test_item(2008 - 5 - 22, 68, -30, 68.97287, -30.2387 ).
+test_item(2008 - 5 - 23, 68, -30, 68.40025, -30.72277 ).
+test_item(2008 - 5 - 24, 68, -30, 68.12665, -30.54753 ).
+test_item(2008 - 5 - 25, 68, -30, 68.94177, -30.18287 ).
+test_item(2008 - 5 - 26, 68, -30, 68.67313, -30.60731 ).
+test_item(2008 - 5 - 27, 68, -30, 68.20968, -30.10144 ).
+test_item(2008 - 5 - 28, 68, -30, 68.68745, -30.21221 ).
+test_item(2008 - 5 - 29, 68, -30, 68.4647, -30.03412 ).
+test_item(2008 - 5 - 30, 68, -30, 68.8531, -30.2446 ).
+test_item(2008 - 5 - 20, 68, -29, 68.63099, -29.61895 ).
+test_item(2008 - 5 - 21, 68, -29, 68.17947, -29.86154 ).
+test_item(2008 - 5 - 22, 68, -29, 68.97287, -29.2387 ).
+test_item(2008 - 5 - 23, 68, -29, 68.40025, -29.72277 ).
+test_item(2008 - 5 - 24, 68, -29, 68.12665, -29.54753 ).
+test_item(2008 - 5 - 25, 68, -29, 68.94177, -29.18287 ).
+test_item(2008 - 5 - 26, 68, -29, 68.67313, -29.60731 ).
+test_item(2008 - 5 - 27, 68, -29, 68.12537, -29.57711 ).
+test_item(2008 - 5 - 28, 68, -29, 68.71044, -29.11273 ).
+test_item(2008 - 5 - 29, 68, -29, 68.27833, -29.74114 ).
+test_item(2008 - 5 - 30, 68, -29, 68.32272, -29.70458 ).
 
 
+test_hashatom :-
+	hashatom_test_case(Dt, Grat, HashAtom),
+	call(hash_atom(Dt, Grat, GotAtom)),
+	(   GotAtom = HashAtom  -> true ;
+		 format('hash_atom ~w ~w expeected ~q got ~q~n', [Dt, Grat, HashAtom, GotAtom])
+	),
+	fail.
+test_hashatom.
 
+% East rule still uses current date for string!
+hashatom_test_case(2013 - 5 - 31, graticule(50, 8), '2013-05-31-15306.02').  % east rule
+hashatom_test_case(2013 - 6 - 6 , graticule(37, -120), '2013-06-06-14955.45').
+
+% peeron map has small 'toggle debug info' in LL corner. Very useful.
+%
+% some stock prices
+% 2013-05-31  15322.22
+% 2013-05-30  15306.02
+% 2013-06-06  14955.45
 
 
 
