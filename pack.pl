ab39773b989cb3fe19df28a56326c2319d6d3e44
Wouter Beek <me@wouterbeek.com>
2013-07-02 11:31:52 +0200

    Added Prolog Pack file to refer pack_install/1 requests to the releases branch (master is used for continuous development).

---
+++ pack.pl
@@ -0,0 +1,8 @@
+author('Anne Ogborn', 'anniepoo@yahoo.com').
+branch_name(release).
+download('https://github.com/Anniepoo/weblog.git').
+home('https://github.com/Anniepoo/weblog').
+name(weblog).
+packager('Wouter Beek', 'me@wouterbeek.com').
+title('weblog').
+
