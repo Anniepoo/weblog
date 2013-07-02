73d1e3e0a629f2cfdfb689c1e6993d935770326f
Wouter Beek <me@wouterbeek.com>
2013-07-02 11:27:13 +0200

    Included Prolog Pack file.

---
+++ pack.pl
@@ -0,0 +1,9 @@
+author('Anne Ogborn', 'anniepoo@yahoo.com').
+branch_name(release).
+download('https://github.com/Anniepoo/weblog.git').
+home('https://github.com/Anniepoo/weblog').
+name(weblog).
+packager('Wouter Beek', 'me@wouterbeek.com').
+title('weblog').
+version('0.0.1').
+
