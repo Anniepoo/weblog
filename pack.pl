75c4044f962fae7bba5b759b78a8cffa3b807654
Wouter Beek <me@wouterbeek.com>
2013-07-04 18:46:59 +0200

    Removed Pack file again.

--- pack.pl
+++
@@ -1,8 +0,0 @@
-author('Anne Ogborn', 'anniepoo@yahoo.com').
-branch(release).
-download('https://github.com/Anniepoo/weblog.git').
-home('https://github.com/Anniepoo/weblog').
-name(weblog).
-packager('Wouter Beek', 'me@wouterbeek.com').
-title('weblog').
-version('0.0.2').
