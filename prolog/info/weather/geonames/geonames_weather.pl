7953d0558515fe708299af89fa9f810af157d738
Anniepoo <annie66us@yahoo.com>
2013-09-22 13:48:33 -0700

    initial non working commit of geonames

--- prolog/info/weather/geonames/geonames_weather.pl
+++ prolog/info/weather/geonames/geonames_weather.pl
@@ -2,8 +2,11 @@
 /** <module>  Geonames implementation of weather
 
 
+this returns the astergdem height at that point
 10 ?- http_get('http://api.geonames.org/astergdemJSON?lat=50.01&lng=10.2&username=anniepoo&style=full', Reply, []).
 Reply = json([astergdem=192,lng=10.2,lat=50.01]).
+
+
 
 */
 :- use_module(library(http/http_client)).
@@ -13,10 +16,13 @@ Reply = json([astergdem=192,lng=10.2,lat=50.01]).
 
 :- ensure_loaded(weblog(resources/resources)).
 
+% TBD - this has a defect. settings are per-module, but
+% we want all settings shared for geonames
+%
 :- setting(
   geonames_username,
   atom,
-  'iamnotavalidkeypasteoneinhere',
+  'notarealgeonameskey',
   'Geonames key.  Get one at http://www.geonames.org/'
 ).
 
@@ -26,16 +32,22 @@ prolog:message(missing_key_file(File)) -->
   % Print an error message if the keyfile is not present.
   (
     absolute_file_name(
-      weblog('keys/geonamekey'),
+      weblog('keys/geonameskey'),
       File,
       [access(read), file_errors(fail), file_type(prolog)]
     )
   ->
     load_settings(File)
   ;
-    print_message(warning, missing_key_file('geonamekey.pl'))
+    debug(weblog, 'Geonames key missing', [])
   ).
 
+geo_weather_widget(_) -->
+  {
+    setting(geonames_username, notarealgeonameskey)
+  },
+  !,
+  html([p('Missing geonames key in weblog/keys/geonameskey.pl')]).
 geo_weather_widget(Generator) -->
 	{
 	    setting(geonames_username, UserName),
@@ -43,13 +55,11 @@ geo_weather_widget(Generator) -->
 	    generator_lat_long(Generator, Lat, Lng),
             uri_query_components(Qs, [lat(Lat), lng(Lng), username(UserName)]),
             format(atom(URI), 'http://api.geonames.org/astergdemJSON?~w', [Qs]),
-            http_get(URI, Reply, [])
+            http_get(URI, _Reply, [])
         },
 	html(div([id=ID, class=[weblog, 'weather-widget']],
 		 p('someday a weather widget')
 		)).
-
-
 
 generator_lat_long(Generator, Lat, Lng) :-
 	call(Generator, point(Lat, Lng)).
