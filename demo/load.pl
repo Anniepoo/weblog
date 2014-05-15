7d8d46706d52c60d29ca714c90d890e2f745f2df
Wouter Beek <me@wouterbeek.com>
2014-05-15 18:02:10 +0200

    MINOR: use_module/1 for loading modules; use initialization/1 to auto-run predicate upon loading module.

--- demo/load.pl
+++ demo/load.pl
@@ -4,9 +4,9 @@
 
    Copyright (c) 2012, University of Houston
    Which has released it under the terms of the LGPL
-
-
 */
+
+:- initialization(init_weblog_demo).
 
 init_weblog_demo:-
   % The use of `file_search_path(weblogtest, .)` is unreliable,
@@ -20,20 +20,14 @@ init_weblog_demo:-
   assert(user:file_search_path(js, weblog('static/js'))),
   assert(user:file_search_path(icons, weblog('static/icons'))).
 
-:- init_weblog_demo.
-
 % Turn on the logger
-:- ensure_loaded(library(http/http_log)).
+:- use_module(library(http/http_log)).
 % Turn on sessions
-:- ensure_loaded(library(http/http_session)).
+:- use_module(library(http/http_session)).
 % Needed for http:location/3, don't remove even if red
 :- use_module(library(http/http_path)).
 
 % these are suspicious
 %http:location(admin, '/admin' , []).
 %http:location(demo, '/demo', []).
-
-
-
-
 
