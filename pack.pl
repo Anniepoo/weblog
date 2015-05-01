f873d71d5af79bbaff937d4147938a820c663938
Wouter Beek <me@wouterbeek.com>
2015-05-01 22:15:28 +0200

    ADDED: Updated Prolog package metadata for the upcoming release.

--- pack.pl
+++ pack.pl
@@ -1,8 +1,11 @@
 author('Anne Ogborn', 'anniepoo@yahoo.com').
+author('Thanos Tintinidis', 'thanosqr@gmail.com').
+author('Wouter Beek', 'me@wouterbeek.com').
 download('https://github.com/Anniepoo/weblog.git').
 home('https://github.com/Anniepoo/weblog').
+maintainer('Anne Ogborn', 'anniepoo@yahoo.com').
+maintainer('Wouter Beek', 'me@wouterbeek.com').
 name(weblog).
 packager('Wouter Beek', 'me@wouterbeek.com').
 title('weblog').
-version('0.0.5').
-
+version('0.1.0').
