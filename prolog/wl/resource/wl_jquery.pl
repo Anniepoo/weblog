bb3cd2074c1e74b7ab9137e32d698e3f800f9c2c
Wouter Beek <me@wouterbeek.com>
2015-06-23 15:15:03 +0200

    ENHANCED: Allow jQuery to be loaded in Prolog-based HTML generation           in production (minimized) and in debug mode.

--- prolog/wl/resource/wl_jquery.pl
+++ prolog/wl/resource/wl_jquery.pl
@@ -4,19 +4,31 @@
 
 Defines the HTML resources for including jQuery into a Web page.
 
-Not called `jquery` which is part of SWI-Prolog's HTTP library.
-
 @author Anne Ogborn
 @author Wouter Beek
 @license Lesser General Public License Vers. 3, June 2007.
 @version 2015
 */
 
-:- use_module(library(http/html_head)).
+:- use_module(library(debug)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_server_files)).
 
+:- dynamic(user:file_search_path/2).
+:- multifile(user:file_search_path/2).
 
+user:file_search_path(css, library(wl/resource/css)).
 
-:- html_resource(
-  wl_jquery,
-  [requires(['http://code.jquery.com/jquery-2.1.4.js']),virtual(true)]
-).
+:- http_handler(css(.), serve_files_in_directory(css), [prefix]).
+
+:- if(debugging(js(jquery))).
+  :- html_resource(
+    js(jquery),
+    [requires([js('jquery-2.1.4.js')]),virtual(true)]
+  ).
+:- else.
+  :- html_resource(
+    js(jquery),
+    [requires([js('jquery-2.1.4.min.js')]),virtual(true)]
+  ).
+:- endif.
--- prolog/wl/resource/wl_jquery_ui.pl
+++ prolog/wl/resource/wl_jquery_ui.pl
@@ -10,13 +10,21 @@ Defines the HTML resources for including jQueryUI into a Web page.
 @version 2015
 */
 
-:- use_module(library(http/html_head)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_server_files)).
 :- use_module(library(wl/resource/wl_jquery)).
 
+:- dynamic(user:file_search_path/2).
+:- multifile(user:file_search_path/2).
 
+user:file_search_path(css, library(wl/resource/css)).
+user:file_search_path(js, library(wl/resource/js)).
+
+:- http_handler(css(.), serve_files_in_directory(css), [prefix]).
+:- http_handler(js(.), serve_files_in_directory(js), [prefix]).
 
 :- html_resource(
-  wl_jquery_ui_css,
+  css('jquery-ui'),
   [
     requires(['https://code.jquery.com/ui/1.11.4/themes/ui-lightness/jquery-ui.css']),
     virtual(true)
@@ -24,13 +32,9 @@ Defines the HTML resources for including jQueryUI into a Web page.
 ).
 
 :- html_resource(
-  wl_jquery_ui,
+  js('jquery-ui'),
   [
-    requires([
-      wl_jquery,
-      wl_jquery_ui_css,
-      'http://code.jquery.com/ui/1.11.4/jquery-ui.js'
-    ]),
+    requires([js(jquery),css('jquery-ui'),'http://code.jquery.com/ui/1.11.4/jquery-ui.js']),
     virtual(true)
   ]
 ).
